acd80ed10c45b078304fb81a5257cf3a12acd7af
Kevin Behrens <43488774+agapetry@users.noreply.github.com>
2026-04-20 13:50:01 -0400

    copyright date

--- PublishPress_Statuses.php
+++ PublishPress_Statuses.php
@@ -3,7 +3,7 @@
  * @package PublishPress
  * @author  PublishPress
  *
- * Copyright (c) 2025 PublishPress
+ * Copyright (c) 2026 PublishPress
  *
  * ------------------------------------------------------------------------------
  * Portions of this code were originally derived from the Edit Flow plugin
